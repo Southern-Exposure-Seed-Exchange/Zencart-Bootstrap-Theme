5e69bbd4559245d11d09f9bc051b80e2b27d113b
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2016-04-06 18:18:08 -0400

    Customize the Gift Voucher Redeem Page

---
+++ theme/templates/sese-bootstrap/templates/tpl_gv_redeem_default.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Page Template
+ *
+ * Display information related to GV redemption (could be redemption details, or an error message)
+ *
+ * @package templateSystem
+ * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: tpl_gv_redeem_default.php 4155 2006-08-16 17:14:52Z ajeh $
+ */
+?>
+<div class="centerColumn" id="gvRedeemDefault">
+
+<div class='page-header'><h1 id="gvRedeemDefaultHeading"><?php echo HEADING_TITLE; ?></h1></div>
+
+<div id="gvRedeemDefaultMessage" class="content"><?php echo sprintf($message, $_GET['gv_no']); ?></div>
+
+<p id="gvRedeemDefaultMainContent" class="content"><?php echo TEXT_INFORMATION; ?></p>
+
+<p class='clearfix'>
+<?php echo '<a class="btn btn-primary" href="' . ($_GET['goback'] == 'true' ? zen_href_link(FILENAME_GV_FAQ) : zen_href_link(FILENAME_DEFAULT)) . '">' . BUTTON_CONTINUE_ALT . '</a>'; ?>
+</p>
+
+</div>
