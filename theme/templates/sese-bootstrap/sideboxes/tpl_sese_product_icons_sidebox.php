1ff2c2d63a7024142a8e5efb47967514c03766ca
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2016-06-09 16:54:29 -0400

    Turn the Sidebar's Product Icon Labels Into Links

--- theme/templates/sese-bootstrap/sideboxes/tpl_sese_product_icons_sidebox.php
+++ theme/templates/sese-bootstrap/sideboxes/tpl_sese_product_icons_sidebox.php
@@ -5,54 +5,54 @@ $content = <<<HTML
 <ul class='media-list'>
 <!-- Organic -->
 <li class='media'>
-  <div class='media-left media-middle'>
-    <a href="index.php?main_page=products_all&organic=1">
+  <a href="index.php?main_page=products_all&organic=1">
+    <div class='media-left media-middle'>
       <img class='media-object' src="{$product_icons['organic']['image']}"
            alt="{$product_icons['organic']['title']}" />
-    </a>
-  </div>
-  <div class='media-body'>
-    <h5 class='media-heading'>{$product_icons['organic']['title']}</h5>
-  </div>
+    </div>
+    <div class='media-body'>
+      <h5 class='media-heading'>{$product_icons['organic']['title']}</h5>
+    </div>
+  </a>
 </li>
 
 <!-- Heirloom -->
 <li class='media'>
-  <div class='media-left media-middle'>
-    <a href="index.php?main_page=products_all&heirloom=1">
+  <a href="index.php?main_page=products_all&heirloom=1">
+    <div class='media-left media-middle'>
       <img class='media-object' src="{$product_icons['heirloom']['image']}"
            alt="{$product_icons['heirloom']['title']}" />
-    </a>
-  </div>
-  <div class='media-body'>
-    <h5 class='media-heading'>{$product_icons['heirloom']['title']}</h5>
-  </div>
+    </div>
+    <div class='media-body'>
+      <h5 class='media-heading'>{$product_icons['heirloom']['title']}</h5>
+    </div>
+  </a>
 </li>
 
 <!-- South-East -->
 <li class='media'>
-  <div class='media-left media-middle'>
-    <a href="index.php?main_page=products_all&southern=1">
+  <a href="index.php?main_page=products_all&southern=1">
+    <div class='media-left media-middle'>
       <img class='media-object' src="{$product_icons['southeast']['image']}"
            alt="{$product_icons['southeast']['title']}" />
-    </a>
-  </div>
-  <div class='media-body'>
-    <h5 class='media-heading'>{$product_icons['southeast']['title']}</h5>
-  </div>
+    </div>
+    <div class='media-body'>
+      <h5 class='media-heading'>{$product_icons['southeast']['title']}</h5>
+    </div>
+  </a>
 </li>
 
 <!-- Eco -->
 <li class='media'>
-  <div class='media-left media-middle'>
-    <a href="index.php?main_page=products_all&eco=1">
+  <a href="index.php?main_page=products_all&eco=1">
+    <div class='media-left media-middle'>
       <img class='media-object' src="{$product_icons['eco']['image']}"
            alt="{$product_icons['eco']['title']}" />
-    </a>
-  </div>
-  <div class='media-body'>
-    <h5 class='media-heading'>{$product_icons['eco']['title']}</h5>
-  </div>
+    </div>
+    <div class='media-body'>
+      <h5 class='media-heading'>{$product_icons['eco']['title']}</h5>
+    </div>
+  </a>
 </li>
 
 </ul>
