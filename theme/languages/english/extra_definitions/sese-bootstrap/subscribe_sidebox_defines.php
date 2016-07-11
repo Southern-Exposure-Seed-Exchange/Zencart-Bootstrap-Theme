80c6ffe79b48bfa59c86f075468a431ac5ce7e78
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2016-07-11 18:59:10 -0400

    Update Newsletter Sidebox Text

--- theme/languages/english/extra_definitions/sese-bootstrap/subscribe_sidebox_defines.php
+++ theme/languages/english/extra_definitions/sese-bootstrap/subscribe_sidebox_defines.php
@@ -1,6 +1,6 @@
 <?php
 /** Language Definitions for the subscribe Sidebox */
 define('BOX_HEADING_SUBSCRIBE_SIDEBOX', 'Newsletter');
-define('SUBSCRIBE_SIDEBOX_TEXT', 'Subscribe to our newsletter to receive our latest Garden Guides and updates about our upcoming Events.');
+define('SUBSCRIBE_SIDEBOX_TEXT', 'Subscribe to receive our latest Garden Guides and event updates.');
 define('SUBSCRIBE_SIDEBOX_BUTTON_TEXT', 'Subscribe');
 ?>
