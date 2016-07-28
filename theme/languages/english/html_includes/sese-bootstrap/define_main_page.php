6933c63cf82e0bec0a36862d20ab004c98ada5b6
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2016-07-28 05:06:07 -0400

    Update Home Page

--- theme/languages/english/html_includes/sese-bootstrap/define_main_page.php
+++ theme/languages/english/html_includes/sese-bootstrap/define_main_page.php
@@ -1,150 +1,160 @@
+<!--
+  This page now uses CSS classes included with our theme.
+  Check out http://getbootstrap.com/css/ and http://getbootstrap.com/components/ for available classes.
+-->
 <div class='row'>
 <!-- Warning Banner
 <strong><div style="color:red;">We are currently experiencing technical difficulties with our Fax line. Please order via website or telephone: (540)894-9480 Mon-Fri 9am-5pm
 </div></strong>
 -->
+
 <!-- Main Content -->
 <div class='col-md-9'>
-  <div class='clearfix'>
-    <a href="/catalog-information-ezp-16.html">
-      <img class='pull-left' style="width: 140px;" src="/images/home/sese-catalog-cover-2015-home.jpg"
-          alt="Southern Exposure Catalog Seeds 2015" />
+
+  <!-- Summer/Fall Items -->
+  <h3>Don't forget to order for summer and fall planting!</h3>
+  <p class='text-center'>
+    <a href="/chesnok-red-hardneck-garlic-8-oz-p-248.html">
+      <img style="margin: 3px; height: 120px;" alt="Chesnok Red Hardneck Garlic" src="/images/mivagraphics/veg/garlic-hardneck-chesnokred.jpg" />
     </a>
-    <p>
-      <a href="/2015-catalog-garden-guide-rush-delivery-p-1640.html" target="_blank">
-        Click here to request a 2015 Catalog &amp; Garden Guide.</a>
-      You can also view this year's catalog
-      <a href="/catalog/sese-catalog-2015-web.pdf" target="_blank">online as a PDF</a>.
-    </p>
-    <p>For fastest service, <strong>we recommend  ordering online</strong>.
-      Browse the listings at the left to fill your cart or
-      <a href="/index.php?main_page=quick_order">Quick Order by Catalog Number</a>.
-      If you need to mail in your order instead, you can print this year’s
-      <a href="/catalog/2015-order-form-southern-exposure.pdf">mail-in order form</a>.
-    </p>
+    <a href="/inchelium-red-softneck-garlic-8-oz-p-1180.html">
+      <img style="margin: 3px; height: 120px;" alt="Inchelium Red Garlic" src="/images/veg/garlic-inchelium-red.jpg" />
+    </a>
+    <a href="/italian-softneck-garlic-8-oz-p-51.html">
+      <img style="margin: 3px; height: 120px;" alt="Italian Softneck Garlic" src="/images/garlic-italiansoftneck.jpg" />
+    </a>
+  </p>
+  <p>
+    <a href="/garlic-c-25.html">Garlic</a>,
+    <a href="/perennial-onions-c-120.html">perennial onions</a>,
+    <a href="/index.php?main_page=advanced_search_result&search_in_description=1&keyword=panax">ginseng</a>,
+    and <a href="/goldenseal-10-rhizomes-p-986.html">goldenseal</a> ship in fall
+    for fall planting. Also, don't forget to order
+    <a href="/vegetables-lettuce-c-3_34.html">lettuce</a>,
+    <a href="/vegetables-greens-c-3_30.html">greens</a>,
+    <a href="/vegetables-carrots-c-3_10.html">carrots</a>,
+    <a href="/vegetables-radishes-c-3_47.html">radishes</a> and other crops for
+    summer and fall planting. Check out our
+    <a href="/growing-guides/fall-winter-quick-guide.pdf">Fall and Winter Quick Reference PDF</a>
+    for recommended last planting dates and particularly cold-resistant varieties.
+  </p>
+  <p class='text-center'>
+    <a href="/index.php?main_page=advanced_search_result&search_in_description=1&keyword=tatsoi+rapa">
+      <img style="margin: 3px; height: 120px;" alt="Tatsoi Brassica Rapa" src="/images/veg/tatsoi-mustard.jpg" />
+    </a>
+    <a href="/index.php?main_page=advanced_search_result&search_in_description=1&keyword=silverbeet">
+      <img style="margin: 3px; height: 120px;" alt="Rainbow Chard" src="/images/veg/rainbow-chard.jpg" />
+    </a>
+    <a href="/index.php?main_page=advanced_search_result&search_in_description=1&keyword=sword+leaf">
+      <img style="margin: 3px; height: 120px;" alt="Sword Leaf lettuce" src="/images/veg/sword-leaf-lettuce.jpg" />
+    </a>
+  </p>
+
+  <hr />
+
+  <!-- HHF -->
+  <div class='text-center'>
+    <img class='img-responsive img-center' src="https://www.southernexposure.com/images/home/hhf-2015-speakers.jpg" />
+    <p>Taste 100+ tomatoes, peppers and melons, join us for the Old Timey Seed Swap, and choose from 80+ workshops and demonstrations.</p>
+    <p><strong>General Admission is Saturday, September 10, with pre- and post-festival events <em>Friday–Sunday</em></strong></p>
+    <p><strong><span style="color:#4a4a4a; text-align:center; font-size:16px;"><a style="color:#007700;" href="http://heritageharvestfestival.com/">www.heritageharvestfestival.com</a></span></strong></p>
   </div>
 
   <hr />
 
-  <h3 class='text-center'>New Varieties for 2015</h3>
-  <p class='text-center'>
-    <a href="/luffa-ridged-edible-gourd-3-g-p-1834.html">
-      <img style="height: 120px; margin: 2px;" alt="Mexican Mint Marigold"
-           src="images/veg/ridge-gourds-on-cattle-panel-cropped.jpg" />
-    </a>
-    <a href="/jewels-of-opar-greens-015g-p-1824.html">
-      <img style="height: 120px; margin: 2px;" alt="Jewels of Opar"
-           src="/images/veg/jewels-of-opar.jpg" />
-    </a>
-    <a href="/charleston-hot-pepper-hot-05-g-p-1826.html">
-      <img style="height: 120px; margin: 2px;" alt="Charleston Hot pepper"
-           src="/images/veg/charleston-hot-pepper.jpg" />
-    </a>
-  </p>
-
-  <p>
-    For 2015 we're adding 29 heirloom and organic varieties to our listings.
-    <a href="/jewels-of-opar-greens-015g-p-1824.html" target="_blank">Jewels of Opar</a>
-    is both an elegant flower and a succulent summer leaf vegetable. Flavorful,
-    attractive, bug-resistant <a href="/luffa-ridged-edible-gourd-3-g-p-1834.html" target="_blank">Ridge Gourds</a>
-    can replace zucchini or okra in recipes. We've
-    also added <a href="/charleston-hot-pepper-hot-05-g-p-1826.html" target="_blank">Charleston Hot</a>
-    and <a href="/cayenne-carolina-pepper-hot-05-g-p-1825.html" target="_blank">Carolina Cayenne</a>,
-    two hot peppers bred for resistance to nematodes, making them well-suited
-    to the Southeast.
-  </p>
-
-  <p class='text-center'>
-    <a href="/pennsylvania-dutch-crookneck-squash-winter-3-g-p-1793.html">
-      <img style="margin: 2px; height: 120px;" alt="Pennsylvania Dutch Crookneck Winter Squash"
-           src="/images/veg/pa-dutch-crookneck.jpg" />
-    </a>
-    <a href="/garden-huckleberry-009-g-p-1880.html">
-      <img style="margin: 2px; height: 120px;" alt="Garden Huckleberries"
-           src="/images/veg/garden-huckleberries.jpg" />
-    </a>
-    <a href="/index.php?main_page=advanced_search_result&search_in_description=1&keyword=24111">
-      <img style="margin: 2px; height: 120px;" alt="Yellow Cabbage Collards"
-           src="/images/veg/yellow-cabbage-collards.jpg" />
-    </a>
-  </p>
-
-  <p>
-    Keep eating from your garden all year with crops for canning, storage, and
-    winter harvest. High-yielding
-    <a href="/garden-huckleberry-009-g-p-1880.html" target="_blank">Garden Huckleberries</a>
-    make a great jam.
-    <a href="/pennsylvania-dutch-crookneck-squash-winter-3-g-p-1793.html" target="_blank">Pennsylvania Dutch Crookneck winter squash</a>
-    is a butternut-line squash with excellent storage quality. Rare, tender
-    family heirloom collard greens
-    <a href="/alabama-blue-collards-2-g-p-1803.html" target="_blank">Alabama Blue</a>
-    and
-    <a href="/index.php?main_page=advanced_search_result&search_in_description=1&keyword=24111" target="_blank">Yellow Cabbage Collards</a>
-    are traditional southern winter cooking greens.  You can also try
-    <a href="/austrian-winter-peas-cover-crop-228-g-p-1813.html" target="_blank">Austrian Winter peas</a>
-    for a great nitrogen-fixing cover crop that also adds flavor to salads all winter long in zones 7 and warmer.
-  </p>
-
-  <hr />
-
-  <div class='clearfix'>
-    <h4><a href="/vegetable-gardening-in-the-southeast-the-timber-press-guide-to-p-1722.html">
-      <img class='pull-right' style="margin: 4px 8px; width: 100px;"
-           alt="Timber Press Guide to Vegetable Gardening in the Southeast"
-           src="/images/dvd/books-gardeninginthesoutheast.jpg"></a>
-      Local Authors with New Books to Nurture the Gardener, Farmer &amp; Seedsperson
-    </h4>
-
-    <p>
-      <a href="/vegetable-gardening-in-the-southeast-the-timber-press-guide-to-p-1722.html">
+  <!-- Books -->
+  <div class='media'>
+    <div class='media-body'>
+      <h4 class='media-heading'>Local Authors with New Books to Nurture the Gardener, Farmer & Seedsperson</h4>
+      <a href="http://www.southernexposure.com/vegetable-gardening-in-the-southeast-the-timber-press-guide-to-p-1722.html">
         <em>The Timber Press Guide to Vegetable Gardening in the Southeast</em></a>,
       by Southern Exposure’s own gardening expert Ira Wallace, gives home
       gardeners the regionally specific information needed to succeed in our hot,
       humid climate. It features what to plant, when to plant it, and when to
       harvest, based on our climate, weather, and other info.
-    </p>
-    <p>
-      <a href="/epic-tomatoes-how-to-select-and-grow-the-best-varieties-book-p-1797.html">
-        <img src="/images/dvd/books-epictomatoes.jpg" alt="Epic Tomatoes"
-             class='pull-left' style="width:100px; margin:4px 8px;" />
-      </a><a href="/epic-tomatoes-how-to-select-and-grow-the-best-varieties-book-p-1797.html">
-        Epic Tomatoes: How to Select and Grow the Best Varieties of All Time
-      </a>: Craig Lehoullier  has grown thousands of tomato varieties, most of
-      them in hot and humid North Carolina, and here he shares his hard-won
-      wisdom on how to grow great tasting tomatoes.
-    </p>
-    <p>
-      <a href="/edible-landscaping-with-a-permaculture-twist-book-p-1823.html">
-        <img src="/images/dvd/books-ediblelandscapingwpermaculture.jpg"
-             alt="Edible Landscaping with a Permaculture Twist" class='pull-right'
-             style="width:100px; margin:4px 8px;border:1px solid #666;" />
+    </div>
+    <div class='media-right'>
+      <a href="/vegetable-gardening-in-the-southeast-the-timber-press-guide-to-p-1722.html">
+        <img class='media-object' style="width:100px" alt="Timber Press Guide to Vegetable Gardening in the Southeast" src="/images/dvd/books-gardeninginthesoutheast.jpg">
       </a>
-      <a href="/edible-landscaping-with-a-permaculture-twist-book-p-1823.html">
-        Edible Landscaping with a Permaculture Twist</a>
-      Transforming an ordinary yard into an edible oasis. More than 200 color
-      photos and illustrations show how permaculture principals scale from
-      small urban mini-gardens to suburban yards, commercial landscapes or
-      small farms.
-    </p>
-    <p>
-      <a href="/sustainable-market-farming-book-p-1676.html">
-        <img src="/images/dvd/book-sustainable-market-farming.jpg"
-             alt="Veg Gardening in the Southeast"
-             class='pull-left' style="width:100px; margin:4px 8px; border:1px solid #666;" />
-      </a>
-      Our neighbor Pam Dawling’s <em><a href="/sustainable-market-farming-book-p-1676.html">Sustainable Market Farming</a></em>
-      provides invaluable expertise that is especially useful to those in the
-      Southeast and mid-Atlantic, who can benefit from her many years growing
-      in our bio-region.
-    </p>
-    <p><a href="/books-dvds-c-14.html">Check out all the great books in the SESE library.</a></p>
+    </div>
   </div>
+  <div class='media'>
+    <div class='media-left'>
+      <a href="http://www.southernexposure.com/epic-tomatoes-how-to-select-and-grow-the-best-varieties-book-p-1797.html">
+        <img class='media-object' src="/images/dvd/books-epictomatoes.jpg" alt="Epic Tomatoes" style="width:100px;" />
+      </a>
+    </div>
+    <div class='media-body'>
+      <a href="http://www.southernexposure.com/epic-tomatoes-how-to-select-and-grow-the-best-varieties-book-p-1797.html">
+        Epic Tomatoes: How to Select and Grow the Best Varieties of All Time</a>:
+      Craig Lehoullier  has grown thousands of tomato varieties, most of them
+      in hot and humid North Carolina, and here he shares his hard-won wisdom
+      on how to grow great tasting tomatoes.
+    </div>
+  </div>
+  <div class='media'>
+    <div class='media-body'>
+      <a href="http://www.southernexposure.com/edible-landscaping-with-a-permaculture-twist-book-p-1823.html">
+        Edible Landscaping with a Permaculture Twist</a> Transforming an
+      ordinary yard into an edible oasis. More than 200 color photos and
+      illustrations show how permaculture principals scale from small urban
+      mini-gardens to suburban yards, commercial landscapes or small farms.
+    </div>
+    <div class='media-right'>
+      <a href="http://www.southernexposure.com/edible-landscaping-with-a-permaculture-twist-book-p-1823.html">
+        <img class="media-object" src="/images/dvd/books-ediblelandscapingwpermaculture.jpg" alt="Edible Landscaping with a Permaculture Twist" style="width:100px; border:1px solid #666;" />
+      </a>
+    </div>
+  </div>
+  <div class='media'>
+    <div class='media-left'>
+      <a href="http://www.southernexposure.com/sustainable-market-farming-book-p-1676.html">
+        <img class='media-object' src="/images/dvd/book-sustainable-market-farming.jpg" alt="Veg Gardening in the Southeast" style="width:100px; border:1px solid #666;" />
+      </a>
+    </div>
+    <div class='media-body'>
+      Our neighbor Pam Dawling’s
+      <em><a href="http://www.southernexposure.com/sustainable-market-farming-book-p-1676.html">
+        Sustainable Market Farming</a></em> provides invaluable expertise that
+      is especially useful to those in the Southeast and mid-Atlantic, who
+      can benefit from her many years growing in our bio-region.
+    </div>
+  </div>
+  <p style='margin-top:15px' class='text-center'><a href="http://www.southernexposure.com/books-dvds-c-14.html">Check out all the great books in the SESE library.</a></p>
+
 </div>
 
 <hr class='visible-xs visible-sm' />
 
 <!-- Right Column -->
 <div class='col-md-3'>
+
+  <!-- Catalog -->
+  <a href="http://www.southernexposure.com/catalog-information-ezp-16.html">
+    <img class='img-responsive' src="/images/catalog-2016-cover-80kb.jpg" alt="Southern Exposure Catalog Seeds 2015" />
+  </a>
+  <p>
+    <a href="http://www.southernexposure.com/request-a-catalog-c-215.html" target="_blank">Request a Catalog & Garden Guide.</a>
+    You can also view it <a href="http://www.southernexposure.com/catalog/sese-catalog-2016-for-web.pdf" target="_blank">online as a PDF</a>.
+  </p>
+  <p>
+    For fastest service, <strong>we recommend ordering online</strong>. Browse
+    the listings at the left to fill your cart or
+    <a href="http://www.southernexposure.com/index.php?main_page=quick_order">
+      Quick Order by Item Number</a>.
+    If you need to mail in your order instead, you can print our
+    <a href="http://www.southernexposure.com/catalog/2016-order-form-southern-exposure.pdf">
+      mail-in order form</a>.
+  </p>
+
+  <a href="http://dynalog.catalogs.com/g489/southern-exposure-seed-exchange?merchantID=379">
+    <img class='img-responsive img-center' src="/images/home/mobile-friendly-flipbook-button.png">
+  </a>
+
+  <hr />
+
+  <!-- Garden Planner -->
   <a href="/gardenplanner">
     <img src="/newsletter/2012-december/sese-garden-planner-screenshots.jpg"
          alt="Southern Exposure Garden Planner" class='img-responsive img-center' />
@@ -160,6 +170,7 @@
 
   <hr />
 
+  <!-- Virginia Mix -->
   <a href="/virginia-heritage-seed-collection-p-1314.html">
     <img  src="/images/veg/virginia-heritage-seed-sampler.jpg" alt="Virginia Heritage Seed Sampler" class='img-responsive img-center'/>
   </a>
@@ -170,6 +181,7 @@
 
   <hr />
 
+  <!-- Plant a Row -->
   <h4 class='text-center'>Plant A Row for the Hungry</h4>
   <p><a href="/plant-a-row-ezp-164.html">
     Read about our project, and how to start your own <span class='glyphicon glyphicon-menu-right'></span>
@@ -177,6 +189,7 @@
 
   <hr />
 
+  <!-- Events -->
   <h4 class='text-center'>Events</h4>
   <p>We  host seed swaps, tastings, and workshops on heirloom gardening, year-round edible gardening, and more! </p>
   <p><a href="/events-ezp-40.html">Check out upcoming events <span class='glyphicon glyphicon-menu-right'></span></a></p>
