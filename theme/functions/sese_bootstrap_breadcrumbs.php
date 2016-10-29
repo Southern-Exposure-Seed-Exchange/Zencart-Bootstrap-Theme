b0593d1234f6a8c29f8aecdcb635fe44be3e8b3b
Pavan Rikhi <pavan.rikhi@gmail.com>
2016-10-28 23:00:26 -0400

    Skip Rendering of Breadcrumbs with Blank Titles

--- theme/functions/sese_bootstrap_breadcrumbs.php
+++ theme/functions/sese_bootstrap_breadcrumbs.php
@@ -4,10 +4,20 @@ class BootstrapBreadcrumbs
 {
   /** Generate the Breadcrumbs HTML for the current page.
     *
+    * Removes any crumbs with an empty `title` field.
+    *
     * @param array $breadcrumbs The global breadcrumbs array for the page.
     * @return string The HTML representing the breadcrumbs.
     */
   public static function render($breadcrumbs) {
+    $filtered_breadcrumbs = array();
+    foreach ($breadcrumbs->_trail as $breadcrumb) {
+      if ($breadcrumb['title'] !== '') {
+        array_push($filtered_breadcrumbs, $breadcrumb);
+      }
+    }
+    $breadcrumbs->_trail = $filtered_breadcrumbs;
+
     $content = "<ol class='breadcrumb'>\n";
     $crumb_length = count($breadcrumbs->_trail);
     foreach ($breadcrumbs->_trail as $index => $breadcrumb) {
