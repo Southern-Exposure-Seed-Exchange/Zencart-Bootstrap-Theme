be24bf0271621b7b97027bee22fabe8c17a47104
Pavan Rikhi <pavan.rikhi@gmail.com>
2016-11-03 05:53:24 -0400

    Fix Redirect Chain for Authorize.net Seal

--- theme/templates/sese-bootstrap/sideboxes/tpl_logos_sidebox.php
+++ theme/templates/sese-bootstrap/sideboxes/tpl_logos_sidebox.php
@@ -6,7 +6,7 @@ $logo_image_folder = $template->get_template_dir(
 $content = <<<HTML
 <div class='text-center'>
   <script type="text/javascript" language="javascript">var ANS_customer_id="7ea8129e-b54b-494c-9def-27cfc6d5499b";</script>
-  <script type="text/javascript" language="javascript" src="//verify.authorize.net/anetseal/seal.js" ></script>
+  <script type="text/javascript" language="javascript" src="https://verify.authorize.net/anetseal/seal.js" ></script>
 </div><br />
 <a href="//www.facebook.com/pages/Southern-Exposure-Seed-Exchange/353814746253?ref=ts" target='_blank'>
   <img class='img-center img-responsive' src="{$logo_image_folder}/facebook-big-icon.png" alt="Visit Facebook Page" />
